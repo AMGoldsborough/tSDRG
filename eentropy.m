1c21cd1c32b218740945f2a197260a34123be34e
AMGoldsborough <andrew@goldsborough.org.uk>
2017-03-31 15:04:52 +0200

    Entanglement by SVD

---
+++ eentropy.m
@@ -0,0 +1,17 @@
+function ee = eentropy(S)
+% Entanglement (Von Neumann) Entropy
+% input: matrix of singular values S
+% output: the entanglement entropy
+
+% Andrew Goldsborough 12/11/2012
+% function to calculate the entanglement (Von Neumann) entropy. The
+% Equation is ee = - \sum_{a}^{r} s_{a}^{2} log_{2} (s_{a}^{2}) where S =
+% diag(s_{1}, ... ,s_{r})
+
+%create array of singular values
+Sarray = diag(S);
+Sarray(Sarray==0) = [];
+
+%calculate ee
+ee = - sum(Sarray.*Sarray.*log2(Sarray.*Sarray));
+
